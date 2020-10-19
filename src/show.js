83853e1f9416acbef4dbb85a25c35495c7865ebc
Eason Shao <eason.syc@icloud.com>
2020-10-19 13:12:50 +0800

    change print.html.

--- src/show.js
+++ src/show.js
@@ -1,10 +1,10 @@
 let url = document.location.toString();
-let urlParmStr = url.slice(url.indexOf('?') + 1);
-let arr = urlParmStr.indexOf('&');
+let urlParmStr = url.slice(url.indexOf("?") + 1);
+let arr = urlParmStr.split("&");
 let api = parseInt(arr[1], 16);
-let typ = api / 415;
+let typ = Math.floor(api / 415);
 let tim = api % 415;
-let mon = tim / 32;
+let mon = Math.floor(tim / 32);
 let day = tim % 32;
 let numArray = ["undefined", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let foodArray = ["undefined", "自助餐", "套餐A", "套餐A", "套餐A", "套餐B", "盖浇饭", "套餐C", "面档", "套餐B"];
@@ -20,10 +20,8 @@ if ((mon < tom) || ((mon === tom) && (day < tot))){
 } else {
     dynum = new Date(toy + "/" + mon + "/" + day).getDay()
 }
-let food = foodArray[typ]; // Element 1
-let name = arr[0]; // Element 2
-let num = numArray[typ]; // Element 3
-let loc = locArray[typ]; // Element 4
-let wkd = mon + "/" + day + " 周" + weekArray[dynum] // Element 5
-
-// TODO: A lot
+var food = foodArray[typ]; // Element 1
+var name = decodeURI(arr[0]); // Element 2
+var num = numArray[typ]; // Element 3
+var loc = locArray[typ]; // Element 4
+var wkd = mon + "/" + day + " 周" + weekArray[dynum] // Element 5
