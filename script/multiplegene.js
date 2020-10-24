e4ca51bae06ae3b4a473ef737d94edf905cc9086
Eason Shao <eason.syc@icloud.com>
2020-10-24 16:39:11 +0800

    update help and multiple.

---
+++ script/multiplegene.js
@@ -0,0 +1,21 @@
+let gen = document.getElementById("gen");
+gen.onclick = function () {
+    let agr = document.getElementById("agree");
+    let nam = document.getElementById("name").value;
+    let typ = document.getElementById("typ");
+    typ = parseInt(typ.options[typ.selectedIndex].value);
+    let mon = parseInt(document.getElementById("month").value);
+    let day = parseInt(document.getElementById("day").value);
+    if (agr.checked === false) {
+        alert("请先勾选复选框！");
+        location.reload(true);
+    } else if (mon < 1 || mon > 12 || day < 1 || day > 31) {
+        alert("请输入正确信息！");
+        location.reload(true);
+    } else {
+        let tim = mon * 32 + day;
+        let cs = typ * 416 + tim;
+        let api = cs.toString(16);
+        window.open("./../result/print.html?" + nam + "&" + api, "_blank");
+    }
+}
