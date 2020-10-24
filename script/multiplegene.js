b18681de5d3c3ff5130931b8ca7a0329e0393c24
Eason Shao <eason.syc@icloud.com>
2020-10-24 17:22:09 +0800

    update multiple make.

--- script/multiplegene.js
+++ script/multiplegene.js
@@ -2,20 +2,22 @@ let gen = document.getElementById("gen");
 gen.onclick = function () {
     let agr = document.getElementById("agree");
     let nam = document.getElementById("name").value;
-    let typ = document.getElementById("typ");
-    typ = parseInt(typ.options[typ.selectedIndex].value);
-    let mon = parseInt(document.getElementById("month").value);
-    let day = parseInt(document.getElementById("day").value);
+    let smon = parseInt(document.getElementById("smonth").value);
+    let sday = parseInt(document.getElementById("sday").value);
+    let emon = parseInt(document.getElementById("emonth").value);
+    let eday = parseInt(document.getElementById("eday").value);
+    let typ = document.getElementById("typ").value;
     if (agr.checked === false) {
         alert("请先勾选复选框！");
         location.reload(true);
-    } else if (mon < 1 || mon > 12 || day < 1 || day > 31) {
-        alert("请输入正确信息！");
-        location.reload(true);
+        // } else if (mon < 1 || mon > 12 || day < 1 || day > 31) {
+        //     alert("请输入正确信息！");
+        //     location.reload(true);
     } else {
-        let tim = mon * 32 + day;
-        let cs = typ * 416 + tim;
-        let api = cs.toString(16);
-        window.open("./../result/print.html?" + nam + "&" + api, "_blank");
+        let name = nam;
+        let date = smon * 32 + sday;
+        let len = 1;
+        let api = typ;
+        window.open("./../result/multiple.html?name=" + name + "&date=" + date + "&len=" + len + "&api=" + api, "_blank");
     }
 }
