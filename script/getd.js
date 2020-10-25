fccaa18e90723f6d21b323e18446d2864655424a
jiubugaosunila <eason.syc@icloud.com>
2020-10-25 10:53:04 +0800

    update apis.

--- script/getd.js
+++ script/getd.js
@@ -1,3 +1,4 @@
 let today = new Date();
+var toyear = today.getFullYear();
 var tomonth = today.getMonth() + 1;
 var todate = today.getDate();
--- script/multiplegene.js
+++ script/multiplegene.js
@@ -2,8 +2,10 @@ let gen = document.getElementById("gen");
 gen.onclick = function () {
     let agr = document.getElementById("agree");
     let nam = document.getElementById("name").value;
+    let syear = parseInt(document.getElementById("syear").value);
     let smon = parseInt(document.getElementById("smonth").value);
     let sday = parseInt(document.getElementById("sday").value);
+    let eyear = parseInt(document.getElementById("eyear").value);
     let emon = parseInt(document.getElementById("emonth").value);
     let eday = parseInt(document.getElementById("eday").value);
     let typ = document.getElementById("typ").value;
@@ -13,8 +15,8 @@ gen.onclick = function () {
         alert("请输入正确信息！");
     } else {
         let name = nam;
-        let sdate = smon * 32 + sday;
-        let edate = emon * 32 + eday;
+        let sdate = syear * 416 + smon * 32 + sday;
+        let edate = eyear * 416 + emon * 32 + eday;
         let api = typ;
         window.open("./../result/multip.html?name=" + name + "&sdate=" + sdate + "&edate=" + edate + "&api=" + api, "_blank");
     }
