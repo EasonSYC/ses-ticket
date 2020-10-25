078f965bacc6289cf0632904292121b877f380da
jiubugaosunila <eason.syc@icloud.com>
2020-10-25 17:42:29 +0800

    update android download.

--- script/jump.js
+++ script/jump.js
@@ -3,7 +3,7 @@ function single_jump() {
     if (/iphone|ipad|ipod/.test(userAgent)) {
         window.open("https://www.icloud.com/shortcuts/4fa7687bc26b408b9e1157b75b1e6866", "_blank");
     } else if (/android/.test(userAgent)) {
-        window.open("generate/single.html", "_blank");
+        window.open("https://cowtransfer.com/s/9f5ba5a3853240", "_blank");
     } else {
         window.open("generate/single.html", "_blank");
     }
