6a9187d978eb5bcce8b262a1bba7a5df541dad13
jiubugaosunila <eason.syc@icloud.com>
2020-10-25 17:15:52 +0800

    update jump.

---
+++ script/jump.js
@@ -0,0 +1,10 @@
+function single_jump() {
+    let userAgent = navigator.userAgent.toLowerCase();
+    if (/iphone|ipad|ipod/.test(userAgent)) {
+        window.open("https://www.icloud.com/shortcuts/4fa7687bc26b408b9e1157b75b1e6866", "_blank");
+    } else if (/android/.test(userAgent)) {
+        window.open("generate/single.html", "_blank");
+    } else {
+        window.open("generate/single.html", "_blank");
+    }
+}
