a620a6659ff972e5abc845973ffaf06175ffb03c
jiubugaosunila <eason.syc@icloud.com>
2020-10-25 11:51:21 +0800

    update multiple results.

--- script/showmult.js
+++ script/showmult.js
@@ -22,8 +22,9 @@ let edt = ed % 32;
 
 let numArray = ["undefined", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let foodArray = ["undefined", "自助餐", "套餐A", "套餐A", "套餐A", "套餐B", "盖浇饭", "套餐C", "面档", "套餐B"];
+let locArray = ["undefined", "一", "一", "一", "二", "二", "二", "二", "二", "二"];
 
-var ret = "<h3>" + nam + "的餐票</h3><br>";
+var ret = "";
 
 if (syr > eyr || ((syr === eyr) && ((smo > emo) || ((smo === emo) && (sdt > edt))))) {
     alert("请输入正确日期！");
@@ -48,15 +49,57 @@ if (syr > eyr || ((syr === eyr) && ((smo > emo) || ((smo === emo) && (sdt > edt)
             let ddy = d.getDate();
             let are = numArray[chc];
             let typ = foodArray[chc];
+            let loc = locArray[chc];
             let ntim = dmo * 32 + ddy;
             let ncs = dyr * 416 + ntim;
             let ncss = ncs * 10 + chc;
             let napi = ncss.toString(16);
-            let url = "\"" + "./../result/print.html?" + nam + "&" + napi + "\"";
+            let purl = "\"" + "./../result/print.html?" + nam + "&" + napi + "\"";
+            let surl = "\"" + "./../result/scan.html?" + nam + "&" + napi + "\"";
 
-            ret += "<p>" + dyr + "/" + dmo + "/" + ddy + " " + are + "区" + typ + "的餐票：" +
-                "<a href=" + url + ">点击此处</a>" +
-                "</p>";
+            // ret += "<p>" + dyr + "/" + dmo + "/" + ddy + " " + are + "区" + typ + "的餐票：" +
+            //     "<a href=" + url + ">点击此处</a>" +
+            //     "</p>";
+
+            ret +=
+                "<div class=\"col-6 mt-2 d-flex justify-content-center\">" +
+                "<div class=\"order-panel\"> " +
+                "<div class=\"d-flex justify-content-between\">" +
+                "<div>" +
+                "<div class=\"order-name\">" +
+                typ +
+                "</div>" +
+                "<img class=\"img-qrcode border\" id = \"qrc\" src=\"\" alt=\"\">" + //TODO: Change It
+                "<div class=\"user-name text-center\">" +
+                "<span>" +
+                nam +
+                "</span>" +
+                "</div>" +
+                "</div>" +
+                "<div class=\"ticket-body\">" +
+                "<div class=\"order-area\">" +
+                are +
+                "<span class=\"order-area-tail\">" +
+                "区" +
+                "</span>" +
+                "</div>" +
+                "<div class=\"order-location\">" +
+                "<span>" +
+                "食堂" +
+                loc +
+                "楼" +
+                "<span>" +
+                "<br>" +
+                "<span>" +
+                "10/19 周一" + //TODO: Change it
+                "</span>" +
+                "</span>" +
+                "</span>" +
+                "</div>" +
+                "</div>" +
+                "</div>" +
+                "</div>" +
+                "</div>"
         }
     }
 }
