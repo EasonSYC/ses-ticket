e1cccf7e9ce261744d0fa39d5197ae36c6b5ca3c
jiubugaosunila <eason.syc@icloud.com>
2020-10-26 18:28:33 +0800

    update iOS.

--- script/jump.js
+++ script/jump.js
@@ -1,7 +1,7 @@
 function single_jump() {
     let userAgent = navigator.userAgent.toLowerCase();
     if (/iphone|ipad|ipod/.test(userAgent)) {
-        window.open("https://www.icloud.com/shortcuts/ba04b04dc72049da9c83585581eba8d8", "_blank");
+        window.open("https://www.icloud.com/shortcuts/f45977a24a74441997606c89398b8993", "_blank");
     } else if (/android/.test(userAgent)) {
         window.open("https://cowtransfer.com/s/9f5ba5a3853240", "_blank");
     } else {
