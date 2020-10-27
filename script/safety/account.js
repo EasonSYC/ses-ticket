901b0c09e9a7f715c764e7a74884640428a5ab2f
Eason Shao <eason.syc@icloud.com>
2020-10-27 21:38:09 +0800

    update usrs.

--- script/safety/account.js
+++ script/safety/account.js
@@ -18,19 +18,23 @@ function exists() {
     if (cook === "") return 0;
     let acc = sha1(decodeURI(cook.split("@")[0]));
     let pwd = cook.split("@")[1];
-    let num = 5;
+    let num = 7;
     let nameArray = [
         "d033e22ae348aeb5660fc2140aec35850c4da997",
         "a28079aa9ed5861504d0fde60544f715df764110",
         "fab60904d36306295b5d27dc1745a645482b2201",
         "032b83ec07d20c839f43b18b85ee2a71906cc524",
-        "ca13078b652648bbb734212811a238bf6b835b0f"];
+        "ca13078b652648bbb734212811a238bf6b835b0f",
+        "a67a98a27ce25528db145277dde1a9eee29e9a46",
+        "a7a12ba71f7ab5331cfffe1a0c1e12baa2661069"];
     let pwdArray = [
         "d033e22ae348aeb5660fc2140aec35850c4da997",
         "2e92ac4a17994f037461eb2b476415b11bab3319",
         "7e226f1ff2de32b0e5b998c7b462110c1a21ea4b",
         "c02e02219ecaef0bcdc95d447a6aa44b2d206e32",
-        "b60964940dd43924f6b17ebad061f41caa117b32"];
+        "b60964940dd43924f6b17ebad061f41caa117b32",
+        "01c88f65f014d24283c2794a3de117cebe267d19",
+        "7319be0502b21f03904b0b680e3c44a4ed266d14"];
     for (let i = 0; i < num; ++i)
         if (acc === nameArray[i] && pwd === pwdArray[i])
             return 1;
