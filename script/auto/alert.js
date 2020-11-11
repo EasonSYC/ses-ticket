a19dc70396ba2a61097c29412cb5326f897832db
JasonYing <13761266671@163.com>
2020-11-11 20:36:38 +0800

    update script/auto/alert.js.

--- script/auto/alert.js
+++ script/auto/alert.js
@@ -1,15 +1,21 @@
 function gAlert(txt) {
+    document.getElementById("gAlertModal").style.display = "block";
+    document.getElementById("gAlertBack").style.display = "block";
     document.getElementById("gAlertMsg").innerHTML = txt;
     document.getElementById("gAlertModal").classList.add("show");
     document.getElementById("gAlertBack").classList.add("show");
 }
 
 document.getElementById("gAlertButton").onclick = function(){
+    document.getElementById("gAlertModal").style.display = "none";
+    document.getElementById("gAlertBack").style.display = "none";
     document.getElementById("gAlertModal").classList.remove("show");
     document.getElementById("gAlertBack").classList.remove("show");
 }
 
 document.getElementById("gAlertBack").onclick = function(){
+    document.getElementById("gAlertModal").style.display = "none";
+    document.getElementById("gAlertBack").style.display = "none";
     document.getElementById("gAlertModal").classList.remove("show");
     document.getElementById("gAlertBack").classList.remove("show");
 }
@@ -18,17 +24,17 @@ document.getElementById("gAlertBack").onclick = function(){
 
 code needed to be added before-hand
 
-<div id="gAlertModal" class="modal fade global-alert" style="display: block;">
+<div id="gAlertModal" class="modal fade global-alert" style="display: none;">
     <div class="modal-dialog modal-sm">
         <div class="modal-content">
             <button id="gAlertButton" type="button" class="close float-right">
                 <span aria-hidden="true">×</span>
             </button>
-            <div id="gAlertMsg" class="text-center font-weight-bold">您本周没有预订</div>
+            <div id="gAlertMsg" class="text-center font-weight-bold"></div>
         </div>
     </div>
 </div>
 
-<div id="gAlertBack" class="modal-backdrop fade"></div>
+<div id="gAlertBack" class="modal-backdrop fade" style="display: none;"></div>
 
 */
