9b6c7a4d4f76a2a5538071ba02cb5bca0316d4fa
SES-1987_admin <littleeason@foxmail.com>
2020-11-14 15:31:18 +0800

    update values.

--- script/show/print.js
+++ script/show/print.js
@@ -8,10 +8,6 @@ let mod = tim % 416;
 let mon = Math.floor(mod / 32);
 let day = mod % 32;
 let typ = api % 10;
-let numArray = ["undefined", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let foodArray = ["undefined", "自助餐", "套餐A", "套餐A", "套餐A", "套餐B", "盖浇饭", "套餐C", "面档", "套餐B"];
-let locArray = ["undefined", "一", "一", "一", "二", "二", "二", "二", "二", "二"];
-let weekArray = ["日", "一", "二", "三", "四", "五", "六"];
 let dynum = new Date(yr + '/' + mon + '/' + day).getDay();
 var truurlqr = url.replace("print", "scan");
 truurlqr = decodeURI(truurlqr);
@@ -24,7 +20,6 @@ let nam2 = decodeURI(cook.split("@")[0]);
 
 if (truname !== nam2) {
     gAlert("姓名不匹配！");
-    alert("姓名不匹配！");
     setTimeout(function () {
         window.history.back();
     }, 3000);
