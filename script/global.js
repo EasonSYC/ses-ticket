77ca936d76263927c69e38c3d57c1e790eb1a1df
Eason Shao <eason.syc@icloud.com>
2020-11-27 22:56:27 +0800

    updage global.js.

--- script/global.js
+++ script/global.js
@@ -376,5 +376,5 @@ var typArray = [
 
 // Food Arrays
 
-var foodArray = ["undefined", "自助餐", "套餐A", "套餐A", "套餐A", "套餐B", "盖浇饭", "套餐C", "面档", "套餐B"];
+var foodArray = ["undefined", "套餐A", "套餐A", "套餐A", "套餐B", "套餐B", "盖浇饭", "套餐C", "面档", "套餐B"];
 var locArray = ["undefined", "一", "一", "一", "二", "二", "二", "二", "二", "二"];
