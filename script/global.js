5f59de1115ef2a21cd4c17920474eb294464956c
Eason Shao <eason.syc@icloud.com>
2020-11-27 22:54:45 +0800

    updage global.js.

--- script/global.js
+++ script/global.js
@@ -1,8 +1,6 @@
 // Food Arrays
 
 var numArray = ["undefined", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var foodArray = ["undefined", "自助餐", "套餐A", "套餐A", "套餐A", "套餐B", "盖浇饭", "套餐C", "面档", "套餐B"];
-var locArray = ["undefined", "一", "一", "一", "二", "二", "二", "二", "二", "二"];
 var weekArray = ["日", "一", "二", "三", "四", "五", "六"];
 
 // 404 Arrays
@@ -375,3 +373,8 @@ var typArray = [
     2,
     2,
     1];
+
+// Food Arrays
+
+var foodArray = ["undefined", "自助餐", "套餐A", "套餐A", "套餐A", "套餐B", "盖浇饭", "套餐C", "面档", "套餐B"];
+var locArray = ["undefined", "一", "一", "一", "二", "二", "二", "二", "二", "二"];
