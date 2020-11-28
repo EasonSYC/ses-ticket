299633390f4a6f7507fcc4494b3aeea43768aa38
ses-1987_admin <littleeason@foxmail.com>
2020-11-28 14:26:57 +0800

    update print api - multiple not changed.

--- script/show/scan.js
+++ script/show/scan.js
@@ -1,27 +1,17 @@
-let url = document.location.toString();
-let urlParmStr = url.slice(url.indexOf("?") + 1);
-let arr = urlParmStr.split("&");
-
-let
+let arr = basicURLInfo().parmArr;
 
 let date = decodeDate(arr[1]);
 let yr = date.yr;
 let mo = date.mo;
 let da = date.da;
 
-let api = parseInt(arr[1], 16);
-let tim = Math.floor(api / 10);
-let yr = Math.floor(tim / 416);
-let mod = tim % 416;
-let mon = Math.floor(mod / 32);
-let day = mod % 32;
-let typ = api % 10;
+let typ = parseInt(arr[2]);
 
-let dynum = new Date(yr + '/' + mon + '/' + day).getDay();
-var food = foodArray[typ];
+let dynum = new Date(yr + '/' + mo + '/' + da).getDay();
+
 var name = decodeURI(arr[0]);
 var num = numArray[typ];
+var food = foodArray[typ];
 var loc = "食堂" + locArray[typ] + "楼";
-var wkd = mon + "/" + day + " 周" + weekArray[dynum];
-
-url = decodeURI(url);
+var wkd = mo + "/" + da + " 周" + weekArray[dynum];
+var url = decodeURI(basicURLInfo().url);
