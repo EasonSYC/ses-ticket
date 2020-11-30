a9096e6b642d59e2b6d94d9a098b05e60aa9ef9d
ses-1987_admin <littleeason@foxmail.com>
2020-11-30 19:57:28 +0800

    update - fix allowance bugs.

--- script/show/print.js
+++ script/show/print.js
@@ -8,18 +8,18 @@ let da = date.da;
 let typ = parseInt(arr[1]);
 
 var name = getName();
-var num = numArray[typ];
+var food = foodArray[typ];
 
 var ok = 0;
 
-if (!getUserInfo("name", name, "allow").includes(num)) {
+if (!getUserInfo("name", name, "allow").includes(food)) {
     gAlert("权限不足以生成该餐票！");
     setTimeout(function () {
         window.history.back();
     }, 3000);
 } else {
     let dynum = new Date(yr + '/' + mo + '/' + da).getDay();
-    var food = foodArray[typ];
+    var num = numArray[typ];
     var loc = "食堂" + locArray[typ] + "楼";
     var wkd = mo + "/" + da + " 周" + weekArray[dynum];
     var urlqr = basicURLInfo().url.replace("print", "scan").split("?")[0] + "?" + name + "&" + basicURLInfo().urlParmStr;
