ff606f09bc0d7f706921bb2a3491f9f79e061e02
Eason Shao <eason.syc@icloud.com>
2020-12-17 19:43:42 +0800

    update areas & fix print bug.

--- script/show/scan.js
+++ script/show/scan.js
@@ -21,8 +21,8 @@ function decodeDate(api) {
 var numArray = ["undefined", "1", "1", "2", "3", "4", "5", "5", "6", "7", "8", "9"];
 var weekArray = ["日", "一", "二", "三", "四", "五", "六"];
 
-var foodArray = ["undefined", "套餐A", "面档", "套餐A", "套餐A", "套餐B", "套餐B", "盖浇饭", "盖浇饭", "套餐C", "面档", "套餐B"];
-var locArray = ["undefined", "一", "一", "一", "一", "二", "二", "二", "二", "二", "二", "二"];
+var foodArray = ["undefined", "自助餐", "套餐A", "套餐A", "套餐A", "套餐B", "盖浇饭", "套餐C", "面档", "套餐B"];
+var locArray = ["undefined", "一", "一", "一", "二", "二", "二", "二", "二", "二"];
 
 let arr = basicURLInfo().parmArr;
 
