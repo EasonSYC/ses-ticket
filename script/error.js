32452094b36b83a886f230bd08f3846b55144ccb
Jason-Ying <jasonying2021@gmail.com>
2020-12-19 10:30:26 +0800

    add script/error.js. add more errors

---
+++ script/error.js
@@ -0,0 +1,15 @@
+let but = document.getElementById("but");
+
+if (document.referrer === '') {
+    but.innerHTML = before + home + after;
+} else {
+    but.innerHTML = before + back + after;
+}
+
+let errorIdGet = document.location.href.split("#")[1];
+let i;
+for(i = 0; i < errorId.length(); ++i){
+    if(errorId[i] === errorIdGet) break;
+}
+document.getElementById("error-prompt").innerHTML = errorPrompt[i];
+document.getElementById("error-typ").innerHTML = errorId[i];
