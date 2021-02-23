ec3879cb1b75e238bb02a7ca9719c1b771730f26
Jason-Ying <jasonying2021@gmail.com>
2021-02-23 19:57:54 +0800

    fix bug

---
+++ script/cache.js
@@ -0,0 +1,6 @@
+function cacheset() {
+    let cacheClear = document.getElementById("cache").value;
+    if (cacheClear < 1) gAlert("缓存清空时间过短，请重新设置！");
+    if (cacheClear > 24) gAlert("缓存清空时间过长，请重新设置！");
+    gAlert("缓存清空时间已设置为：" + cacheClear + "小时");
+}
