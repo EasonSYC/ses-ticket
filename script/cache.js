ced675545e83c436fca26e598347d9595ce54dbd
Jason-Ying <jasonying2021@gmail.com>
2021-02-23 19:59:42 +0800

    fix bug again & 500 pushes!

--- script/cache.js
+++ script/cache.js
@@ -1,5 +1,5 @@
 function cacheset() {
-    let cacheClear = document.getElementById("cache").value;
+    var cacheClear = document.getElementById("cache").value;
     if (cacheClear < 1) gAlert("缓存清空时间过短，请重新设置！");
     if (cacheClear > 24) gAlert("缓存清空时间过长，请重新设置！");
     gAlert("缓存清空时间已设置为：" + cacheClear + "小时");
