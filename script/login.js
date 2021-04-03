b0d680e6182a5568d7d05388532d4222e079996f
Eason Shao <eason.syc@icloud.com>
2021-04-03 08:39:46 +0800

    update login.js: add timeline which is deleted.

--- script/login.js
+++ script/login.js
@@ -7,9 +7,10 @@ log.onclick = function () {
     setCache(0, 0);
     let expdate = getCache();
     let d = new Date();
+    let timeline = d.getTime();
     d.setTime(d.getTime() + (expdate * 24 * 60 * 60 * 1000));
     let expires = d.toUTCString();
-    let cookie = "acc=" + username + "@" + password + ";expires=" + expires + ";path=/";
+    let cookie = "acc=" + username + "@" + password + "@" + timeline + ";expires=" + expires + ";path=/";
     document.cookie = cookie;
     index();
 }
