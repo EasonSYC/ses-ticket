eb3e16121e26f57a3ddb33a909a5d2e02c7ded7a
EasonSYC <eason.syc@icloud.com>
2021-04-05 17:55:26 +0800

    add qprint logout.

--- script/qprint/qprintglobal.js
+++ script/qprint/qprintglobal.js
@@ -2,6 +2,13 @@ function qprintNoLogin() {
     window.location.replace("/sesticket/qprint/qprint.html");
 }
 
+function delQprintCookie() {
+    let d = new Date();
+    d.setTime(d.getTime() - 1);
+    let c = getCookie("qprint");
+    document.cookie = "qprint" + "=" + c + ";expires=" + d.toUTCString() + ";path=/sesticket/qprint";
+}
+
 function getQprintName() {
     return (getCookie("qprint") === "") ? "-1" : decodeURI(getCookie("qprint").split("@")[0]);
 }
