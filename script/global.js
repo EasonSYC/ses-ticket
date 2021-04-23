5840916e53bd0ee80eda317838d5768d5caa8c82
Eason Shao <eason.syc@icloud.com>
2021-04-23 14:58:21 +0800

    revert.

--- script/global.js
+++ script/global.js
@@ -1,8 +1,6 @@
 let bannedIpLength = 2;
 let bannedIp = ["61.172.251.", "203.156.232.66"];
 
-alert("测试测试");
-
 for (let i = 0; i < bannedIpLength; ++i) {
     if (returnCitySN["cip"].includes(bannedIp[i])) window.location.replace("http://www.ses.sh.edu.cn");
 }
