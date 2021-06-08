62ecda6c93bd3f061733f996f6206d42b43feb0a
Eason Shao <eason.syc@icloud.com>
2021-06-08 18:50:11 +0800

    change the way of using api.

--- script/charge.js
+++ script/charge.js
@@ -1,18 +1,16 @@
-let cgm = chargeModel;
-for (let i = 0; i < chargeID.length; i++) {
-    if (chargeID[i] === getUserInfo("acc", getCookie("acc"), "id")) {
-        let chargeStat, csdate = new Date(chargeStartDate[i]), cedate = new Date(chargeEndDate[i]), date = new Date();
-        if (date >= csdate && date <= cedate) chargeStat = "正常";
-        if (date >= csdate && date >= cedate) chargeStat = "到期";
-        if (date <= csdate && date <= cedate) chargeStat = "未开始";
-        if (date <= csdate && date >= cedate) chargeStat = "数据异常";
-        cgm.replace(/ID/g, sha1(chargeID[i]).substr(0, 6));
-        cgm.replace(/START/g, chargeStartDate[i]);
-        cgm.replace(/END/g, chargeEndDate[i]);
-        cgm.replace(/STAT/g, chargeStat);
-        document.getElementById("charge-list").innerHTML += cgm;
+let tmp = "";
+for (let i = 0; i < chargeUserID.length; i++) {
+    if (chargeUserID[i] === getUserInfo("name", getName(), "id")) {
+        let cgm = chargeModel;
+        let chargeStat = chargeStatList[verifyCharge(getUserInfo("name", getName(), "id"))];
+        cgm = cgm.replace(/ID/g, sha1(i.toString()).substr(0, 6));
+        cgm = cgm.replace(/START/g, chargeStartDate[i]);
+        cgm = cgm.replace(/END/g, chargeEndDate[i]);
+        cgm = cgm.replace(/STAT/g, chargeStat);
+        tmp += cgm;
     }
 }
+document.getElementById("charge-list").innerHTML = tmp;
 
 document.getElementById("charge").onclick = function () {
     gAlert("请联系管理员付款！");
